53ed8eaec1f09d85944a1c097f6a3cb81ad5b955
spike <1335407527@qq.com>
2023-05-03 21:46:41 +0800

    第一次提交
    Signed-off-by: spike <1335407527@qq.com>

---
+++ app/src/main/java/com/example/test001/FunctionActivity00.java
@@ -0,0 +1,14 @@
+package com.example.test001;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FunctionActivity00 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_function00);
+    }
+}
